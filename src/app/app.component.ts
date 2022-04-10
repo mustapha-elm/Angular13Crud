80942260b6d20a80c1bfe72ace41dab2cdd9669a
mustapha <mustapha.elm@hotmail.fr>
2022-04-10 18:20:34 +0200

    finalisation du projet angular crud avec material ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { DialogComponent } from './dialog/dialog.component';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular13Crud';
+
+  displayedColumns: string[] = ['productName', 'category', 'date', 'freshness', 'price', 'comment', 'action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor (private dialog :MatDialog, private api: ApiService) {}
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '35%'
+    }).afterClosed().subscribe(val=>{
+      if(val==='save'){
+        this.getAllProducts()
+      }
+    });
+  }
+
+  getAllProducts() {
+    this.api.getProduct().subscribe({
+      next: (res)=>{
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort
+      },
+      error: ()=>{
+        console.log("error !!");
+      }
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  editProduct(row : any) {
+    this.dialog.open(DialogComponent, {
+      width: '35%',
+      data: row
+    }).afterClosed().subscribe(val=>{
+      if(val==='update'){
+        this.getAllProducts()
+      }
+    });
+  }
+
+  deleteProduct(id : number) {
+    this.api.deleteProduct(id).subscribe({
+      next: (res)=>{
+        alert('Product deleted successfully');
+        this.getAllProducts()
+      } ,
+      error: ()=>{alert('Error while deleting the product')}
+    })
+  }
+
+  ngOnInit() {
+    this.getAllProducts();
+  }
+  
 }
